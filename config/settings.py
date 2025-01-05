2abdbfc40f30f49bb43bd00acd1c05e6e9b1e099
sedin <puppet2s@naver.com>
2025-01-05 20:43:14 +0900

    DATABASES 부분 변경  - mysql 정보 입력해놨으니 그대로 만들어서 연결  - 접속 정보 변경해도 상관없음,  단 변경시 commit에서 제외할 것  - mysql 버전은 8.0.39로 설치하였음(8.0 이상이면 아무거나 상관없을 듯)  - db 연결 후 migrate 해야함

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'testdb',                  
+        'USER': 'labeldata',                          
+        'PASSWORD': 'labeldata1!',                  
+        'HOST': '127.0.0.1',                     
+        'PORT': '3306',         # 기본 포트 주소   
     }
 }
 
