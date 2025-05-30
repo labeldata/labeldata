2feb92044129277a2eee87b0201d8f1e1680cc67
puppet2s <puppet2s@naver.com>
2025-05-31 05:53:58 +0900

    favicon 추가 및 분리배출 마크 리사이즈

--- v1/config/urls.py
+++ v1/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('v1.main.urls')),  # 메인 앱 URL
@@ -9,3 +11,8 @@ urlpatterns = [
     path('disposition/', include('v1.disposition.urls', namespace='disposition')),  # disposition 앱 URL
     path('user-management/', include('v1.user_management.urls', namespace='user_management')),  # user_management 앱
 ]
+
+# 개발 환경에서 static 파일 서빙
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
